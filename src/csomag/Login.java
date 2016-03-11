325ece3df77d9cbfd357342290004af0492add58
Bathory Alex <bathory.alex@gmail.com>
2016-03-11 23:04:36 +0100

    Skeleton of the task

---
+++ src/csomag/Login.java
@@ -0,0 +1,45 @@
+package csomag;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.Response;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by bathoryalex on 3/11/2016.
+ */
+public class Login extends HttpServlet
+{
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        String name = req.getParameter("username");
+        String pass = req.getParameter("password");
+        PrintWriter out = resp.getWriter();
+        HttpSession session = req.getSession();
+
+//        if ((name.equals("alex") && pass.equals("12345")) || (name.equals("coco") && pass.equals("kaka")))
+        if (!name.equals(session.getAttribute("username")))
+        {
+            if (name.equals("Alex") && pass.equals("12345"))
+            {
+                req.getRequestDispatcher("welcome.html").include(req, resp);
+                out.println("Szevasz " + name + "!" + " I hope you don't have a shitty day!");
+                session.setAttribute("username", name);
+            } else
+            {
+                req.getRequestDispatcher("index.html").include(req, resp);
+                out.println("The given username or password is incorrect! Permission denied!");
+            }
+        }
+        else
+        {
+            req.getRequestDispatcher("index.html").include(req, resp);
+            out.println("You already logged in!");
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bathoryalex
+  Date: 3/11/2016
+  Time: 20:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
